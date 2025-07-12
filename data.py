c5de6739069ed9e0e9bc473c9bfee4e4e986b32c
agrimastermind <gopalramlora1982@gmail.com>
2025-07-12 11:07:51 +0530

    Add files via upload

---
+++ data.py
@@ -0,0 +1,42 @@
+# data.py
+
+data = {
+    "B.Sc. Agriculture": {
+        "Syllabus": {
+            "📄 B.Sc Syllabus": {
+                "view": "https://drive.google.com/view-link",
+                "download": "https://drive.google.com/download-link"
+            }
+        },
+        "1st Semester": {
+            "🌾 Fundamentals of Agronomy": {
+                "view": "https://drive.google.com/view-link",
+                "download": "https://drive.google.com/download-link"
+            },
+            "🧪 Introductory Soil Science": {
+                "view": "https://drive.google.com/view-link",
+                "download": "https://drive.google.com/download-link"
+            }
+        }
+    },
+    "RPSC AAO": {
+        "Hindi": {
+            "📁 Agriculture": {
+                "📄 Agronomy Notes": {
+                    "view": "https://...",
+                    "download": "https://..."
+                }
+            }
+        }
+    },
+    "Agriculture Supervisor": {
+        "English": {
+            "📁 GK": {
+                "📄 Rajasthan GK Notes": {
+                    "view": "https://...",
+                    "download": "https://..."
+                }
+            }
+        }
+    }
+}
